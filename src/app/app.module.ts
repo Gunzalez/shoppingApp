714356c270ad7a4d3a5549e6b9c684716f57482b
Segun Konibire <gunzalez@hotmail.com>
2018-04-16 23:23:32 +0100

    fleshes out header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipesListComponent } from './recipes/recipes-list/recipes-list.compon
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipesListComponent,
     RecipesDetailComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    RecipesItemComponent
   ],
   imports: [
     BrowserModule
