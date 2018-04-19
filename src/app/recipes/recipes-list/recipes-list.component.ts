856dd945ae85829e641bf042f3abceed98c5a6c6
Segun Konibire <gunzalez@hotmail.com>
2018-04-19 20:26:08 +0100

    edits header, creates recipe model

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  
+  recipes: Recipe[] = [
+    new Recipe('Sample Recipe', 'This is a test will it work', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/10/mexican-chicken-burger.jpg?itok=_AGDavEw')
+  ]
 
   constructor() { }
 
