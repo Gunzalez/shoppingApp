0d1a39d732c302bab30b29c90ef4984787fceab5
Segun Konibire <gunzalez@hotmail.com>
2018-05-20 07:48:36 +0100

    adds ingredients to recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,31 @@
 
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Sample Recipe', 'This is a test will it work', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/10/mexican-chicken-burger.jpg?itok=_AGDavEw'),
-        new Recipe('Another Sample Recipe', 'Wow it is working', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/10/mexican-chicken-burger.jpg?itok=_AGDavEw')
+        new Recipe(
+            'Beef Burger Recipe', 
+            'Info about this amazing burger',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/1200px-Hamburger_%28black_bg%29.jpg',
+            [
+                new Ingredient('Beef Patty', '1'),
+                new Ingredient('Buns', '2'),
+                new Ingredient('Lettuce', '1')
+            ]
+        ),
+        new Recipe(
+            'Schnitzel',
+            'Background on this meal',
+            'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg',
+            [
+                new Ingredient('Chicken', '1'),
+                new Ingredient('Lemon', '1')
+            ])
     ]
 
     getRecipes(){
